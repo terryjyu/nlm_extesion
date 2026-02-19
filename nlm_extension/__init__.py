43728c939ec3528dcf10f37c3e9a3224464df27f
terryjyu <terry.j.yu@gmail.com>
2026-02-20 03:56:55 +0800

    Align nlm command client with tmc/nlm CLI behavior

---
+++ nlm_extension/__init__.py
@@ -0,0 +1,21 @@
+"""NLM extension integration helpers."""
+
+from .nlm_commands import (
+    AudioAsset,
+    CommandResult,
+    NlmClient,
+    NlmCommandError,
+    NlmErrorKind,
+    NotebookSummary,
+    OverviewJob,
+)
+
+__all__ = [
+    "AudioAsset",
+    "CommandResult",
+    "NlmClient",
+    "NlmCommandError",
+    "NlmErrorKind",
+    "NotebookSummary",
+    "OverviewJob",
+]
